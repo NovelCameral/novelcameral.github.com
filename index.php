ef15f70fa1c8c2c452f2b49cc81ab51f8acb5e5a
NovelCameral <dukai2@huawei.com>
2017-02-19 13:49:01 +0800

    upload the website

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+	<head>
+		<title>today.php</title>
+	</head>
+	<body>
+		<p>
+			<?php
+			print "<b>Welcome to my home page <br /> <br />";
+			print "Today is:</b>";
+			print date("l,F jS");
+			print "<br />";
+			?>
+			hello csp
+		</p>
+	</body>
+</html>
